1c499c72a0bbd0016e916be960ebd55cff06a57a
DEBORA VIRGILIA CANNE <canne@professor.educacao.sp.gov.br>
2021-12-02 17:38:32 -0300

    adicionar teste login

---
+++ cypress/integration/login_test.spec.js
@@ -0,0 +1,9 @@
+/// <reference types = "cypress"/>
+escribe('Teste de Login',() => {
+it('Deve fazer login com sucesso', () => {
+    
+    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    
+});
+
+});
